f3dc54700944699895e6f2e717f1bc029b2403c6
AndreiCalazans <andreixoc@hotmail.com>
2017-03-02 21:39:08 -0300

    made first store dispatch and getState commands

--- app/redux-example.js
+++ app/redux-example.js
@@ -4,9 +4,27 @@ console.log('starting redux example');
 
 var reducer = (state = {name: 'Anonymous'}, action) => {
   // state = state || {name: 'Anonymous'};  es5 syntax
+
+  switch (action.type) {
+    case 'CHANGE_NAME':
+      return {
+        ...state,
+        name: action.name
+      };
+    default:
+      return state;
+  }
   return state;
 };
 var store = redux.createStore(reducer);
 
 var currentState = store.getState();
 console.log("currentState: ",currentState);
+
+var action = {
+  type: 'CHANGE_NAME',
+  name: 'Andrei'
+};
+store.dispatch(action);
+
+console.log('Name should be andrei', store.getState());
--- app/redux-todo-example.js
+++ app/redux-todo-example.js
@@ -10,9 +10,26 @@ var stateDefault = {
 
 var reducer = (state = stateDefault, action) => {
   // state = state || {name: 'Anonymous'};  es5 syntax
+
+  switch (action.type) {
+    case 'CHANGE_SEARCH_TEXT':
+      return {
+        ...state,
+        searchText: action.searchText
+      }
+    default:
+      return state;
+  }
   return state;
 };
 var store = redux.createStore(reducer);
 
 
 console.log("currentState: ",store.getState());
+
+// create an action to change the search text
+store.dispatch({
+  type: 'CHANGE_SEARCH_TEXT',
+  searchText: 'change me'
+});
+console.log('Update state is: ', store.getState());
