de3cb87858ad8f7fd38639132eb9fc24d636a298
AndreiCalazans <andreixoc@hotmail.com>
2017-03-03 12:56:10 -0300

    added suppport for location fetching

--- app/redux-example.js
+++ app/redux-example.js
@@ -1,17 +1,11 @@
 var redux  = require('redux');
-
+var axios = require('axios');
 console.log('starting redux example');
 
-var stateDefault = {
-  name: 'Anonymous',
-  hobbies: [],
-  movies: []
-};
-
-var nextHobbyId = 1;
-var nextMovieId = 1;
 
 
+//Name reducer and action generators
+// ---------------------------------
 var nameReducer = (state = 'Anonymous', action) => {
   switch (action.type) {
     case 'CHANGE_NAME':
@@ -21,7 +15,16 @@ var nameReducer = (state = 'Anonymous', action) => {
 
   }
 };
+var changeName = (name) => {
+  return {
+    type: 'CHANGE_NAME',
+    name      // name: name (using only name is a ES6 syntax)
+  }
+};
 
+//Hobby reducers and action generators
+// ---------------------------------
+var nextHobbyId = 1;
 var hobbiesReducer = (state = [] , action) => {
   switch (action.type) {
     case 'ADD_HOBBY':
@@ -39,6 +42,22 @@ var hobbiesReducer = (state = [] , action) => {
   }
 };
 
+var addHobby = (hobby) => {
+  return {
+    type: 'ADD_HOBBY',
+    hobby
+  }
+};
+var removeHobby = (id) => {
+  return {
+    type: 'REMOVE_HOBBY',
+    id
+  }
+};
+
+//Movie reducers and action generators
+// ---------------------------------
+var nextMovieId = 1;
 var movieReducer = (state = [] , action) => {
   switch (action.type) {
     case 'ADD_MOVIE':
@@ -57,13 +76,79 @@ var movieReducer = (state = [] , action) => {
   }
 }
 
+var addMovie = (name , genre) => {
+  return {
+    type: 'ADD_MOVIE',
+    movies: {
+      name,
+      genre
+    }
+  }
+};
+var removeMovie = (id) => {
+  return {
+    type: 'REMOVE_MOVIE',
+    id
+  }
+};
+
+
+//Map reducer and action generators
+// ---------------------------------
+var mapReducer = (state = {isFething: false, url: undefined}, action) => {
+  switch (action.type) {
+    case 'START_LOCATION_FETCH':
+      return {
+        isFetching: true,
+        url: undefined
+      };
+    case 'COMPLETE_LOCATION_FETCH':
+      return {
+        isFetching: false,
+        url: action.url
+      };
+    default:
+      return state;
+
+  }
+};
+
+
+var startLocationFetch = () => {
+  return {
+    type: 'START_LOCATION_FETCH',
+
+  }
+};
+
+var completeLocationFetch = (url) => {
+  return {
+    type: 'COMPLETE_LOCATION_FETCH',
+    url
+  };
+};
+
+var fetchLocation = () => {
+  store.dispatch(startLocationFetch());
+
+  axios.get('http://ipinfo.io').then((res) => {
+    var loc = res.data.loc;
+    var baseUrl = 'http://maps.google.com?q=';
+
+    store.dispatch(completeLocationFetch(baseUrl + loc));
+  });
+}
+
+// redirecting the use of reducers to each functionality
 var reducer = redux.combineReducers({
   name: nameReducer,
   hobbies: hobbiesReducer,
-  movies: movieReducer
+  movies: movieReducer,
+  map: mapReducer
 });
 
 
+// creating the store and composing to be able to use chrome dev tools for redux
 var store = redux.createStore(reducer, redux.compose(
   window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : f => f
 ));
@@ -75,63 +160,53 @@ var store = redux.createStore(reducer, redux.compose(
 var unsubscribe = store.subscribe( () => {
   var state = store.getState();
 
-  console.log('Name is', state.name);
-  document.getElementById('app').innerHTML   = state.name;
 
   console.log('New state' , store.getState());
+
+  if(state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading...';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a target="_blank" href="'+ state.map.url+'">View your location</a>';
+  }
 });
 // unsubscribe();
 
 var currentState = store.getState();
 console.log('currentState: ', currentState);
 
-var action = {
-  type: 'CHANGE_NAME',
-  name: 'Andrei'
-};
-store.dispatch(action);
 
-store.dispatch({
-  type:'ADD_HOBBY',
-  hobby: 'Running'
-});
-store.dispatch({
-  type:'ADD_HOBBY',
-  hobby: 'walking'
-});
-store.dispatch({
-  type: 'REMOVE_HOBBY',
-  id:2
-});
+// for store.dispatch you can use both formulation of the object using type and name
+// or you can use action generator functions like changeName()
 
-store.dispatch({
-  type: 'ADD_MOVIE',
-  movies: {
-    name: 'spiderman',
-    genre: 'action'
-  }
-})
+fetchLocation();
 
-store.dispatch({
-  type: 'CHANGE_NAME',
-  name: 'John'
-});
+// dispatch with action generators
+store.dispatch(changeName('Andrei'));
 
-store.dispatch({
-  type: 'ADD_MOVIE',
-  movies: {
-    name: 'mad max',
-    genre: 'action'
-  }
-});
+store.dispatch(addHobby('Running'));
 
-store.dispatch({
-  type:'REMOVE_MOVIE',
-  id: 1
-});
+store.dispatch(removeHobby(2));
+store.dispatch(changeName('Raysa'));
+store.dispatch(addMovie('Nemo', 'adventure'));
+store.dispatch(addHobby('walking'));
+store.dispatch(addMovie('mad max', 'action'));
 
+store.dispatch(removeMovie(1));
 
-
+// examples of dispatch without action generators
+// store.dispatch({
+//   type: 'CHANGE_NAME',
+//   name: 'John'
+// });
+// store.dispatch({
+//   type:'ADD_HOBBY',
+//   hobby: 'walking'
+// });
+// var stateDefault = {
+//   name: 'Anonymous',
+//   hobbies: [],
+//   movies: []
+// };
 // var oldreducer = (state = stateDefault, action) => {
 //   // state = state || {name: 'Anonymous'};  es5 syntax
 //
